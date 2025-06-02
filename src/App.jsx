ba7f743f57072ef67a9e7f0c83b89d1ea1f5e384
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-06-02 10:35:11 +0530

    dss

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,33 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Product from "./components/Product";
 import Cart from "./components/Cart";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Logout from "./components/Logout";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createContext } from "react";
+export const AppContext = createContext();
 function App() {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
   return (
     <div>
-      <BrowserRouter>
-        <header>
-          <h1>Manikanta's Online Shop</h1>
-          <Link to="/">Home</Link>-
-          <Link to="/cart">Cart</Link>
-          <hr />
-        </header>
-
-        <main>
+      <AppContext.Provider value={{ users, setUsers, user, setUser }}>
+        <BrowserRouter>
+          <Header />
           <Routes>
             <Route index element={<Product />} />
-            <Route path="/" element={<Product/>}></Route>
-            <Route path="/cart" element={<Cart/>}></Route>
+            <Route path="/" element={<Product />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/logout" element={<Logout />}></Route>
+            <Route path="/register" element={<Register />}></Route>
           </Routes>
-        </main>
-
-        <footer>
-          <hr />
-          &copy; 2005. All rights Reserved.
-        </footer>
-      </BrowserRouter>
+          <Footer />
+        </BrowserRouter>
+      </AppContext.Provider>
     </div>
   );
 }
